fda0b65280ad9040cb56933c9a89ca7ab83e2dd9
Mebabit <langwarmebabit@gmail.com>
2026-06-11 15:33:06 +0530

    Adding q3

---
+++ pyQuestion/q3.py
@@ -0,0 +1,7 @@
+#Input 2 number and store it in two variable A abd B. Write a program in python to swap the number
+a = int(input("Enter the value of A: "))
+b = int(input("Enter the value pf B: "))
+
+a, b = b, a
+
+print(f"After sawapping A = {a} and B = {b}")
