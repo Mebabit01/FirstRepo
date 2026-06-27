c94f651a6933e889d90cb7d90c0477330ec0160b
Mebabit <langwarmebabit@gmail.com>
2026-06-27 15:32:54 +0530

    Adding pattern 13

---
+++ pyPattern/pattern13.py
@@ -0,0 +1,9 @@
+# Pattern - 13
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == 1 or i == 5 or i == j:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
