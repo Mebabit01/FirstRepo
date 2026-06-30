a67201b53a5713ff9997d4c37b9b6fc698e978e9
Mebabit <langwarmebabit@gmail.com>
2026-06-30 11:58:28 +0530

    commiting whatever just to start new things

---
+++ CourseraExcercise/string/string1.py
@@ -0,0 +1,12 @@
+fruit = "banana"
+index = 0
+
+#While loop
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index += 1
+
+#for loop
+for letter in fruit:
+    print(letter)
